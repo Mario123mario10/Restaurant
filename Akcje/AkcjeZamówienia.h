9377b3a3078cbe813c541d6f71e8c28998bc5b13
Kruk <Aleksanderjakubkruk@gmail.com>
2022-05-26 21:03:22 +0200

    kopiowanie

--- Akcje/AkcjeZamówienia.h
+++ Akcje/AkcjeZamówienia.h
@@ -1,7 +1,7 @@
-#include "ObsługaZamówienia.h"
-#include "Akcje.h"
-#include "InneKlasy.h"
 #include <vector>
+#include "ObsługaZamówienia.h"
+#include "Akcja.h"
+#include "InneKlasy.h"
 
 
 class Akcje_dla_zamowienia  //ta klasa ma ustalac kolejne mozliwe statusy zamowienia
@@ -19,18 +19,17 @@ public:
         vector <Akcja> wynikowy;
         Kelner kelner("Jan");
         Akcja_zanies_menu akcja1(aktualne_zamowienie, kelner);
-        Przyjscie_kolejnych_osob_do_tego_samego_stolika akcja2(aktualne_zamowienie, 7);
+        PrzyjscieKolejnychOsobDoTegoSamegoStolika akcja2(aktualne_zamowienie, 7);
         wynikowy.push_back(akcja1);
         wynikowy.push_back(akcja2);
         return wynikowy;
     }
-
     vector<Akcja> akcje_dla_czytania_menu()
     {
-        Zupa z1("krupnik");
+        ZupaSzkic z1("krupnik");
         vector <Akcja> wynikowy;
-        Odebranie_zamowienia_od_stolika akcja1(aktualne_zamowienie, &z1, 4);
-        Przyjscie_kolejnych_osob_do_tego_samego_stolika akcja2(aktualne_zamowienie, 9);
+        OdebranieZamowieniaOdStolika akcja1(aktualne_zamowienie, &z1, 4);
+        PrzyjscieKolejnychOsobDoTegoSamegoStolika akcja2(aktualne_zamowienie, 9);
         wynikowy.push_back(akcja1);
         wynikowy.push_back(akcja2);
         return wynikowy;
@@ -38,13 +37,13 @@ public:
 
     vector<Akcja> akcje_dla_oczekiwania_na_dania()
     {
-        Zupa z1("rosol");
+        ZupaSzkic z1("rosol");
         vector <Akcja> wynikowy;
 
-        Odebranie_zamowienia_od_stolika akcja1(aktualne_zamowienie, &z1, 2); // moze po zamowieniu jeszcze raz zamawiac posilki zeb restauracja wiecej zarobila
-        Przyjscie_kolejnych_osob_do_tego_samego_stolika akcja2(aktualne_zamowienie, 9);
-        Czekanie_na_przyniesienie_dan akcja3(aktualne_zamowienie);
-        Podanie_dan_do_stolika akcja4(aktualne_zamowienie);
+        OdebranieZamowieniaOdStolika akcja1(aktualne_zamowienie, &z1, 2); // moze po zamowieniu jeszcze raz zamawiac posilki zeb restauracja wiecej zarobila
+        PrzyjscieKolejnychOsobDoTegoSamegoStolika akcja2(aktualne_zamowienie, 9);
+        CzekanieNaPrzyniesienieDan akcja3(aktualne_zamowienie);
+        PodanieDanDoStolika akcja4(aktualne_zamowienie);
 
         wynikowy.push_back(akcja1);
         wynikowy.push_back(akcja2);
@@ -56,11 +55,11 @@ public:
     {
         vector <Akcja> wynikowy;
 
-        Zupa z1("grochowka");
-        Odebranie_zamowienia_od_stolika akcja1(aktualne_zamowienie, &z1, 2); // moze po zamowieniu jeszcze raz zamawiac posilki zeb restauracja wiecej zarobila
-        Przyjscie_kolejnych_osob_do_tego_samego_stolika akcja2(aktualne_zamowienie, 7);
-        Trwa_jedzenie_posilku akcja3(aktualne_zamowienie);
-        Zakonczenie_wszystkich_posilkow akcja4(aktualne_zamowienie);
+        ZupaSzkic z1("grochowka");
+        OdebranieZamowieniaOdStolika akcja1(aktualne_zamowienie, &z1, 2); // moze po zamowieniu jeszcze raz zamawiac posilki zeb restauracja wiecej zarobila
+        PrzyjscieKolejnychOsobDoTegoSamegoStolika akcja2(aktualne_zamowienie, 7);
+        TrwaJedzeniePosilku akcja3(aktualne_zamowienie);
+        ZakonczenieWszystkichPosilkow akcja4(aktualne_zamowienie);
 
         wynikowy.push_back(akcja1);
         wynikowy.push_back(akcja2);
@@ -71,8 +70,8 @@ public:
     vector<Akcja> akcje_dla_czekania_na_rachunek()
     {
         vector <Akcja> wynikowy;
-        Czekanie_na_rachunek akcja1(aktualne_zamowienie);
-        Podanie_rachunku_klientom_przez_kelnera akcja2(aktualne_zamowienie);
+        CzekanieNaRachunek akcja1(aktualne_zamowienie);
+        PodanieRachunkuKlientomPrzezKelnera akcja2(aktualne_zamowienie);
 
         wynikowy.push_back(akcja1);
         wynikowy.push_back(akcja2);
@@ -83,7 +82,7 @@ public:
     vector<Akcja> akcje_dla_czekania_kelnera_na_zaplate()
     {
         vector <Akcja> wynikowy;
-        Zaplata_rachunku akcja1(aktualne_zamowienie);
+        ZaplataRachunku akcja1(aktualne_zamowienie);
 
         wynikowy.push_back(akcja1);
         return wynikowy;
@@ -93,7 +92,7 @@ public:
     {
         vector <Akcja> wynikowy;
 
-        Wyjscie_z_restauracji akcja1(aktualne_zamowienie);
+        WyjscieZRestauracji akcja1(aktualne_zamowienie);
 
         wynikowy.push_back(akcja1);
         return wynikowy;
@@ -101,11 +100,12 @@ public:
     vector<Akcja> akcje_dla_wyjscia_z_restauracji()
     {
         vector <Akcja> wynikowy;
-        Przygotowanie_stolu_dla_nowych_klientow akcja1(aktualne_zamowienie);
+        PrzygotowanieStoluDlaNowychKlientow akcja1(aktualne_zamowienie);
 
         wynikowy.push_back(akcja1);
         return wynikowy;
     }
+
     vector<Akcja> akcje_dla_posprzatanego_stolu()
     {
         //stol wraca do stolow czystych w klasie restauracja
@@ -118,39 +118,39 @@ public:
     {
         switch(aktualne_zamowienie.daj_status())
         {
-            case oczekiwanie_na_menu:
+            case SZ::oczekiwanie_na_menu:
             {
                 return akcje_dla_oczekiwania_na_menu();
             }
-            case czytanie_menu:
+            case SZ::czytanie_menu:
             {
                 return akcje_dla_czytania_menu();
             }
-            case oczekiwanie_na_dania:
+            case SZ::oczekiwanie_na_dania:
             {
                 return akcje_dla_oczekiwania_na_dania();
             }
-            case jedzenie:
+            case SZ::jedzenie:
             {
                 return akcje_dla_jedzenia();
             }
-            case czekanie_na_rachunek:
+            case SZ::czekanie_na_rachunek:
             {
                 return akcje_dla_czekania_na_rachunek();
             }
-            case czekanie_kelnera_na_zaplate:
+            case SZ::czekanie_kelnera_na_zaplate:
             {
                 return akcje_dla_czekania_kelnera_na_zaplate();
             }
-            case zaplacone:
+            case SZ::zaplacone:
             {
                 return akcje_dla_zaplacenia();
             }
-            case wyjscie_z_restauracji:
+            case SZ::wyjscie_z_restauracji:
             {
                 return akcje_dla_wyjscia_z_restauracji();
             }
-            case stol_posprzatany:
+            case SZ::stol_posprzatany:
             {
                 return akcje_dla_posprzatanego_stolu();
             }
